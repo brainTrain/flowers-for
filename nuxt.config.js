e126be9e4fc39bd352d056d4756c2e24a7338efb
brainTrain <abductee@gmail.com>
2022-05-30 09:08:48 -0700

    add some vue stuffs

---
+++ nuxt.config.js
@@ -0,0 +1,80 @@
+// lovingly lifted from: https://nuxtjs.org/faq/github-pages/#deploying-to-github-pages-for-repository
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase =
+  process.env.DEPLOY_ENV === "GH_PAGES"
+    ? {
+        router: {
+          base: "/sf-siren/",
+        },
+      }
+    : {};
+
+module.exports = {
+  ...routerBase,
+  telemetry: false,
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildvModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    "@nuxtjs/eslint-module",
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-154723261-1",
+      },
+    ],
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container">
+    <ApproveAudio />
+    <div class="siren-wrapper">
+      <Siren />
+    </div>
+    <div>
+      <h1 class="title">
+        sf-siren
+      </h1>
+      <h2 class="subtitle">
+        Miss the Tuesday noon siren? Get your fix here!
+      </h2>
+      <!--
+      <h2 class="subtitle">
+        <SirenText />
+      </h2>
+      -->
+    </div>
+    <Clock
+      v-if="shouldRenderNewShit"
+      :timezone="America/Tijuana"
+      :interval-time="500"
+      @on-clock-time-change="handleClockTimeChange"
+    >
+      <template v-slot:default="clock">
+        <div>
+          {{ clock.dateTime }}
+        </div>
+      </template>
+    </Clock>
+    <SirenAudio
+      :should-play-audio="isInTimeWindow"
+      :time="dateTime"
+    />
+  </div>
+</template>
+
+<script>
+import ApproveAudio from '~/components/approve-audio.vue'
+import Siren from '~/components/siren.vue'
+import SirenAudio from '~/components/siren-audio.vue'
+// import SirenText from '~/components/siren-text.vue'
+import Clock from '~/components/clock.vue'
+
+export default {
+  components: {
+    ApproveAudio,
+    Siren,
+    SirenAudio,
+    // SirenText
+    Clock
+  },
+  data () {
+    return {
+      dateTime: new Date(),
+      isInTimeWindow: undefined,
+      shouldRenderNewShit: process.env.NODE_ENV === 'development'
+    }
+  },
+  methods: {
+    handleClockTimeChange ({ dateTime, isInTimeWindow }) {
+      // only set value if it's changed
+      if (this.isInTimeWindow !== isInTimeWindow) {
+        this.isInTimeWindow = isInTimeWindow
+        this.dateTime = dateTime
+        // console.log('this.isInTimeWindow', this.isInTimeWindow)
+        // console.log('this.dateTime', this.dateTime)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.siren-wrapper {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+}
+
+.siren-wrapper svg {
+  fill: #CCC;
+  opacity: 0.5;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+  word-break: break-word;
+}
+</style>
