7b4ca8591158c66f070513c93cb4ec306df93d7f
brainTrain <abductee@gmail.com>
2024-02-17 00:12:30 -0800

    hmm

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         </h1>
       </section>
       <section class="flower-wrapper">
-        <img class="flower-image" :src="'/' + month + '-flowers.jpg'">
+        <img class="flower-image" :src="month + '-flowers.jpg'">
       </section>
       <section class="bee-grid">
         <AnimatedCell
